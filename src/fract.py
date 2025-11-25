b10ac060bcf7ccfd0ea30351bd50245f9f48150a
Logan M Jones <loganjones1@mail.weber.edu>
2025-11-25 16:09:32 -0700

    learn arrays

--- src/fract.py
+++ src/fract.py
@@ -2,10 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-
 def f(z):
     return np.square(z) - 1
 
 
-z = [4, 1-0.2j, 1.6]
-f(z)
+
+
+print(a)
+
+
